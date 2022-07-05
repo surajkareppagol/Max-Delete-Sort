40ae94f4e76403e51162330c29288017678275c9
shadoww <dev.shadoww@gmail.com>
2022-07-05 11:05:11 +0530

    Update maxDeletesort.c

--- maxDeletesort.c
+++ maxDeletesort.c
@@ -1,12 +1,12 @@
 /*
  * AUTHOR : SHADOWW
- * LAST MODIFIED : 2 / 07 / 2022
+ * LAST MODIFIED : 5 / 07 / 2022
  */
 
 #include <stdio.h>
 #define MAX 60
 
-int n, i, l, j, x, temp, tempElement, indexNumber, elementList[MAX];
+int n, i, l, j, x, tempElement, indexNumber, elementList[MAX];
 
 int main()
 {
@@ -23,30 +23,20 @@ int main()
 
   for (j = 0; j < n; j++)
   {
-    temp = elementList[0];
+    tempElement = elementList[0];
     x = 0;
     for (i = 1; i < indexNumber; i++)
     {
-      if (temp < elementList[i])
+      if (tempElement < elementList[i])
       {
-        temp = elementList[i];
+        tempElement = elementList[i];
         x = i;
       }
     }
-    if (x == 0)
-    {
-      tempElement = elementList[l];
-      elementList[l] = elementList[0];
-      elementList[0] = tempElement;
-      l -= 1;
-    }
-    else
-    {
-      tempElement = elementList[l];
-      elementList[l] = elementList[x];
-      elementList[x] = tempElement;
-      l -= 1;
-    }
+    tempElement = elementList[l];
+    elementList[l] = elementList[x];
+    elementList[x] = tempElement; // SWAP THE ELEMENTS
+    l -= 1;
     indexNumber -= 1;
   }
 
